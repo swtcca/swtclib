06eb95ed18be9b0baeb5451e6664ad18ab6cde72
GinMu <gin.musheng@gmail.com>
2019-08-12 17:50:30 +0800

    fixed serialize & parse api of STHash128

--- packages/swtc-serializer/src/types/STHash128.ts
+++ packages/swtc-serializer/src/types/STHash128.ts
@@ -1,11 +1,12 @@
-import { isHexInt64String, serializeHex } from "../Utils"
+import BigInteger = require("bn-plus.js")
+import { isString, serializeHex } from "../Utils"
 import SerializedType from "./SerializedType"
 
 const STHash128 = new SerializedType({
   id: 4,
   serialize(so, val) {
     // const hash = UInt128.from_json(val);
-    if (isHexInt64String(val)) {
+    if (isString(val) && /^[0-9A-F]{32}$/i.test(val)) {
       serializeHex(so, val, true) // noLength = true
     } else {
       throw new Error("Invalid Hash128")
@@ -13,15 +14,7 @@ const STHash128 = new SerializedType({
   },
   parse(so) {
     const val = so.read(16)
-    if (!Array.isArray(val) || val.length !== 16) {
-      // this._value = NaN;
-      return NaN
-    } else {
-      // this._value  = new BigInteger([0].concat(j), 256);
-      // TODO: need to verify
-      return NaN // new BigNumber(val, 256);
-    }
-    // return UInt128.from_bytes(so.read(16));
+    return new BigInteger([0].concat(val), 256).toString(16).toUpperCase()
   }
 })
 
