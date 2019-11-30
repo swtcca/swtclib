373691cf8acb15ab4d20f40dce4564fd6ae494cc
Lospring Liu <lospringliu@gmail.com>
2019-11-29 19:47:03 -0500

    rearch v1

--- packages/swtc-proxy/tssrc/web/controllers/v1.ts
+++ packages/swtc-proxy/tssrc/web/controllers/v1.ts
@@ -1,19 +1,19 @@
 import { APIError } from "../rest"
-import * as store from "../../store/index"
+import * as V1 from "../../functions/v1"
 
 module.exports = {
-  "GET /v1/wallet/new": async ctx => ctx.rest(store.Wallet.generate()),
-  "GET /v1/accounts/:address/info": store.getAccountInfo,
-  "GET /v1/accounts/:address/authorizes": store.getAccountAuthorizes,
-  "GET /v1/accounts/:address/freezes": store.getAccountFreezes,
-  "GET /v1/accounts/:address/trusts": store.getAccountTrusts,
-  "GET /v1/accounts/:address/balances": store.getAccountBalances,
-  "GET /v1/accounts/:address/payments": store.getAccountPayments,
-  "GET /v1/accounts/:address/payments/:hash": store.getAccountPayment,
-  "GET /v1/accounts/:address/transactions": store.getAccountTransactions,
-  "GET /v1/accounts/:address/transactions/:hash": store.getAccountTransaction,
-  "GET /v1/accounts/:address/orders": store.getAccountOrders,
-  "GET /v1/accounts/:address/orders/:hash": store.getAccountOrder,
+  "GET /v1/wallet/new": V1.generateWallet,
+  "GET /v1/accounts/:address/info": V1.getAccountInfo,
+  "GET /v1/accounts/:address/authorizes": V1.getAccountAuthorizes,
+  "GET /v1/accounts/:address/freezes": V1.getAccountFreezes,
+  "GET /v1/accounts/:address/trusts": V1.getAccountTrusts,
+  "GET /v1/accounts/:address/balances": V1.getAccountBalances,
+  "GET /v1/accounts/:address/payments": V1.getAccountPayments,
+  "GET /v1/accounts/:address/payments/:hash": V1.getAccountPayment,
+  "GET /v1/accounts/:address/transactions": V1.getAccountTransactions,
+  "GET /v1/accounts/:address/transactions/:hash": V1.getAccountTransaction,
+  "GET /v1/accounts/:address/orders": V1.getAccountOrders,
+  "GET /v1/accounts/:address/orders/:hash": V1.getAccountOrder,
   "POST /v1/accounts/:address/payments": async (ctx, next) => {
     throw new APIError("api:disable", "insecure, use post v1/blob")
   },
@@ -23,19 +23,19 @@ module.exports = {
   "DELETE /v1/accounts/:address/orders": async (ctx, next) => {
     throw new APIError("api:disable", "insecure, disabled")
   },
-  "GET /v1/order_book/:base/:counter": store.getOrderBook,
-  "GET /v1/order_book/bids/:base/:counter": store.getOrderBookBids,
-  "GET /v1/order_book/asks/:base/:counter": store.getOrderBookAsks,
-  "GET /v1/transactions/:hash": store.getTransaction,
+  "GET /v1/order_book/:base/:counter": V1.getOrderBook,
+  "GET /v1/order_book/bids/:base/:counter": V1.getOrderBookBids,
+  "GET /v1/order_book/asks/:base/:counter": V1.getOrderBookAsks,
+  "GET /v1/transactions/:hash": V1.getTransaction,
   "POST /v1/accounts/:address/contract/deploy": async (ctx, next) => {
     throw new APIError("api:disable", "insecure, post v1/blob suggested")
   },
   "POST /v1/accounts/:address/contract/call": async (ctx, next) => {
     throw new APIError("api:disable", "insecure, post v1/blob suggested")
   },
-  "GET /v1/ledgers/closed": store.getLedgerClosed,
-  "GET /v1/ledgers/index/:index": store.getLedgerIndex,
-  "GET /v1/ledgers/hash/:hash": store.getLedgerHash,
-  "POST /v1/blob": store.postBlob,
-  "POST /v1/multisign": store.postJsonMultisign
+  "GET /v1/ledgers/closed": V1.getLedgerClosed,
+  "GET /v1/ledgers/index/:index": V1.getLedgerIndex,
+  "GET /v1/ledgers/hash/:hash": V1.getLedgerHash,
+  "POST /v1/blob": V1.postBlob,
+  "POST /v1/multisign": V1.postJsonMultisign
 }
