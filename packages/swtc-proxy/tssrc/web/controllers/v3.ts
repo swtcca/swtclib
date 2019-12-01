0164e191228682d8a0ab82a58a5b0d33bd04c054
Lospring Liu <lospringliu@gmail.com>
2019-12-01 00:11:29 -0500

    update v3 for all ops and param validation

--- packages/swtc-proxy/tssrc/web/controllers/v3.ts
+++ packages/swtc-proxy/tssrc/web/controllers/v3.ts
@@ -1,6 +1,7 @@
 import * as V3 from "../../functions/v3"
 
 module.exports = {
+  "GET /v3/accounts": V3.getAccounts,
   "GET /v3/accounts/:account/info": V3.getAccountInfo,
   "GET /v3/accounts/:account/tums": V3.getAccountTums,
   "GET /v3/accounts/:account/authorizes": V3.getAccountAuthorizes,
@@ -13,8 +14,11 @@ module.exports = {
   "GET /v3/accounts/:account/transactions/:hash": V3.getAccountTransaction,
   "GET /v3/accounts/:account/orders": V3.getAccountOrders,
   "GET /v3/accounts/:account/orders/:hash": V3.getAccountOrder,
+  "GET /v3/accounts/:account/signerlist": V3.getAccountSignerList,
+  "GET /v3/brokers/:account": V3.getBrokerage,
   "GET /v3/order_book/:gets/:pays": V3.getOrderBook,
   "GET /v3/transactions/:hash": V3.getTransaction,
+  "GET /v3/server/info": V3.getServerInfo,
   "GET /v3/ledgers/closed": V3.getLedgerClosed,
   "GET /v3/ledgers/index/:index": V3.getLedgerIndex,
   "GET /v3/ledgers/hash/:hash": V3.getLedgerHash,
