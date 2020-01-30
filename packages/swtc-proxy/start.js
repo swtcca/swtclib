d49ff9bc54f75a7a7482990ba51bf59a74fbba5b
Lospring Liu <lospringliu@gmail.com>
2020-01-30 17:38:53 -0500

    remove eslint and fix vue version

--- packages/swtc-proxy/start.js
+++ packages/swtc-proxy/start.js
@@ -2,9 +2,11 @@ const PROXY = require("./src/index")
 
 // PROXY.state.funcConfig() // 默认基金会测试节点
 // PROXY.state.funcConfig({server: 'ws://139.198.19.175:5055'}) // 指定基金会测试节点
-PROXY.state.funcConfig({ server: "ws://swtcproxy.swtclib.ca:5020" }) // 指定节点
+PROXY.state.funcConfig({
+  server: "ws://swtcproxy.swtclib.ca:5020",
+  debug: true
+}) // 指定节点
 // PROXY.state.funcConfig({ server: "wss://s.jingtum.com:5020" }) // 指定节点
-// PROXY.state.funcConfig({ server: "ws://101.200.230.74:5020" }) // 多签节点
 // PROXY.state.funcConfig({ debug: true }) // 调试
 // PROXY.state.funcConfig({ port: 5080 }) // 端口
 // 设置环境变量控制配置 UPSTREAM->server DEBUG->debug PORT->port
